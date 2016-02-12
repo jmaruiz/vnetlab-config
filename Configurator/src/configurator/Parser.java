56275f9b200f413240929d03926fa1e9260b521e
Jose Ruiz <jmaruiz5@gmail.com>
2016-02-12 10:06:39 -0500

    Rework parser to use hashmap

--- Configurator/src/configurator/Parser.java
+++ Configurator/src/configurator/Parser.java
@@ -1,14 +1,13 @@
 package configurator;
 import java.io.*;
-import java.util.Arrays;
+import java.util.*;
 
 public class Parser {
     public static void main(String [] args) {
 
         // This will reference one line at a time
         String line;
-        Boolean parseObject = false;
-        Object currentItem = new NetworkItem(null, null);
+        HashMap<String, Object> itemsMap = new HashMap<>();
 
         try {
             // FileReader reads text files in the default encoding.
@@ -19,39 +18,37 @@ public class Parser {
 
             while((line = bufferedReader.readLine()) != null) {
                 //System.out.println(line);
-                if (parseObject) {
-                    String[] lineRead = line.split(":");
-                    if (lineRead[0].contains("}")) {
-                        System.out.println("Object ended.");
-                        parseObject = false;
+                String[] lineRead = line.split(" ");
+                if (lineRead.length == 3) {
+                    if (lineRead[2].contains("{")) {
+                        NetworkItem item = new NetworkItem(lineRead[0], lineRead[1]);
+                        itemsMap.put(lineRead[1], item);
                     } else {
-                        //currentItem.setos(lineRead[1]);
+                        
                     }
+                } else if (lineRead.length == 2 && lineRead[0].contains("partial_solution")){
+                    //System.out.println("END FOUND");
+                } else if (line.trim().equals("")) {
+                    //System.out.println("Blank line.");
                 } else {
-                    String[] lineRead = line.split(" ");
-                    if (lineRead.length == 3) {
-                        if (lineRead[2].contains("{")) {
-                            NetworkItem item = new NetworkItem(lineRead[0], lineRead[1]);
-                            parseObject = true;
-                            currentItem = item;
-                        } else {
-
-                        }
-                    } else if (lineRead.length == 2 && lineRead[0].contains("partial_solution")){
-                        System.out.println("END FOUND");
-                    } else if (line.trim().equals("")) {
-                        System.out.println("Blank line.");
-                    } else {
-                        System.out.println("Length incorrect. Actual length: " + lineRead.length);
-                    }
+                    //System.out.println("Length incorrect. Actual length: " + lineRead.length);
                 }
             }   
-
+            
             // Always close files.
-            bufferedReader.close();         
+            bufferedReader.close();
+            printMap(itemsMap);
         }
         catch(IOException ex) {
             System.out.println(ex.getMessage());
+        }
+    } 
+    
+    public static void printMap(Map map) {
+       for (Object name: map.keySet()){
+            String key = name.toString();
+            String value = map.get(name).toString();
+            System.out.println(key + " " + value);  
         }
     }
 }
