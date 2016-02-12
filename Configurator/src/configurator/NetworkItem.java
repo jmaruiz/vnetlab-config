345335dab74a923145233771eebb124b5e7954f0
Jose Ruiz <jmaruiz5@gmail.com>
2016-02-12 11:29:46 -0500

    Added switch to set object variables by parsing parameters

--- Configurator/src/configurator/NetworkItem.java
+++ Configurator/src/configurator/NetworkItem.java
@@ -10,33 +10,46 @@ package configurator;
  * @author joser
  */
 public class NetworkItem {
-    public String type, name, os, ver, src, eth0, eth1, inf, subnet, netmask;
+    public String type, name, os, ver, src, eth0, eth1, eth2, inf, subnet, netmask;
     
     NetworkItem (String t, String n) {
         type = t;
         name = n;
         System.out.println("New object created: " + this.type + " - " + this.name);
     }
-    
+    /*
     public void setos(String s) {
         this.os = s;
+        System.out.println("OS of " + this.name + " set to " + this.os);
     }
     public void setver(String s) {
         this.ver = s;
+        System.out.println("Ver of " + this.name + " set to " + this.ver);
     }
     public void setsrc(String s) {
         this.src = s;
+        System.out.println("Src of " + this.name + " set to " + this.src);
     }
     public void seteth0(String s) {
         this.eth0 = s;
+        System.out.println("Eth0 of " + this.name + " set to " + this.eth0);
     }
     public void seteth1(String s) {
         this.eth1 = s;
+        System.out.println("Eth1 of " + this.name + " set to " + this.eth1);
     }
     public void setinf(String s) {
         this.inf = s;
+        System.out.println("Inf of " + this.name + " set to " + this.inf);
     }
     public void setsubnet(String s) {
         this.subnet = s;
+        System.out.println("Subnet of " + this.name + " set to " + this.subnet);
     }
+    
+    public void setnetmask(String s) {
+        this.subnet = s;
+        System.out.println("Netmask of " + this.name + " set to " + this.netmask);
+    }
+    */
 }
--- Configurator/src/configurator/Parser.java
+++ Configurator/src/configurator/Parser.java
@@ -6,8 +6,11 @@ public class Parser {
     public static void main(String [] args) {
 
         // This will reference one line at a time
-        String line;
-        HashMap<String, Object> itemsMap = new HashMap<>();
+        String line, param, value, curType, curName;
+        String[] lineRead;
+        HashMap<String, NetworkItem> itemsMap = new HashMap<>();
+        NetworkItem curMapItem = null;
+        Boolean findObjects = true;
 
         try {
             // FileReader reads text files in the default encoding.
@@ -18,20 +21,70 @@ public class Parser {
 
             while((line = bufferedReader.readLine()) != null) {
                 //System.out.println(line);
-                String[] lineRead = line.split(" ");
-                if (lineRead.length == 3) {
-                    if (lineRead[2].contains("{")) {
-                        NetworkItem item = new NetworkItem(lineRead[0], lineRead[1]);
-                        itemsMap.put(lineRead[1], item);
+                if (findObjects) {
+                    lineRead = line.split(" ");
+                    if (lineRead.length == 3) {
+                        if (lineRead[2].contains("{")) {
+                            curType = lineRead[0];
+                            curName = lineRead[1];
+                            NetworkItem item = new NetworkItem(curType, curName);
+                            itemsMap.put(curName, item);
+                            curMapItem = itemsMap.get(curName);
+                            findObjects = false;
+                        } else {
+
+                        }
+                    } else if (lineRead.length == 2 && lineRead[0].contains("partial_solution")){
+                        //System.out.println("END FOUND");
+                    } else if (line.trim().equals("")) {
+                        //System.out.println("Blank line.");
                     } else {
-                        
+                        //System.out.println("Length incorrect. Actual length: " + lineRead.length);
                     }
-                } else if (lineRead.length == 2 && lineRead[0].contains("partial_solution")){
-                    //System.out.println("END FOUND");
-                } else if (line.trim().equals("")) {
-                    //System.out.println("Blank line.");
                 } else {
-                    //System.out.println("Length incorrect. Actual length: " + lineRead.length);
+                    System.out.println(line.trim());
+                    lineRead = line.trim().split(":");
+                   if (lineRead.length == 2) {
+                        param = lineRead[0].trim();
+                        value = lineRead[1].trim();
+                        try {
+                            switch(param) {
+                                case "os":
+                                    curMapItem.os = value;
+                                    break;
+                                case "ver":
+                                    curMapItem.ver = value;
+                                    break;
+                                case "src":
+                                    curMapItem.src = value;
+                                    break;
+                                case "eth0":
+                                    curMapItem.eth0 = value;
+                                    break;
+                                case "eth1":
+                                    curMapItem.eth1 = value;
+                                    break;
+                                case "eth2":
+                                    curMapItem.eth2 = value;
+                                    break;
+                                case "inf":
+                                    curMapItem.inf = value;
+                                    break;
+                                case "subnet":
+                                    curMapItem.subnet = value;
+                                    break;
+                                case "netmask":
+                                    curMapItem.netmask = value;
+                                    break;
+                                default:
+                                    System.out.println("Unknown Parameter Error.");
+                            }
+                        } catch(NullPointerException e) {
+                            System.out.println("The object you're trying to access doesn't exist.");
+                        }
+                   } else if (lineRead.length == 1 && lineRead[0].contains("}")) {
+                       findObjects = true;
+                   }
                 }
             }   
             
